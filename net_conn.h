06bed1f78b91cf67b3971523b587f530f3c9f61a
rebornfly <fqhlys@163.com>
2017-11-11 11:25:13 +0800

    Update net_conn.h
    调整格式

--- net_conn.h
+++ net_conn.h
@@ -43,111 +43,112 @@ namespace znb
 		return std::string(::inet_ntoa(addr)); 
 	}
 
-    inline std::string str2Hex(char* data, int len)
-    {
-        char hex[17];
-        memset(hex, 0 ,17);
-        int n = 0;
-        for(int i = 0; i < len; i++)
-        {
-            n += sprintf(hex + n, "%02x ", data[i]);
-        }
-        return hex;
-    }
+	inline std::string str2Hex(char* data, int len)
+	{
+		char hex[17];
+		memset(hex, 0 ,17);
+		int n = 0;
+		for(int i = 0; i < len; i++)
+		{
+		    n += sprintf(hex + n, "%02x ", data[i]);
+		}
+		return hex;
+	}
 
-    class CTcpConn : boost::noncopyable,
+	class CTcpConn : boost::noncopyable,
                       public boost::enable_shared_from_this<CTcpConn>
 	{
-		public:
+	public:
 			
             typedef boost::shared_ptr<CTcpConn> TcpConnPtr;
             typedef boost::function<void (const TcpConnPtr&)> CloseCallback;
 
-			CTcpConn(CEpoll* ep, int so, char* ip, uint32_t port,  IDataHandler* dataHandler):
-			m_pDataHandler(dataHandler),
-			m_uPort(port),
-			m_strIp(ip),
-			m_input(4*1024, 2*1024),
-			m_output(4*1024, 2*1024),
-			m_uLastWarn(0),
-			m_uDropCount(0),
-			m_bOverflow(false),
+	    CTcpConn(CEpoll* ep, int so, char* ip, uint32_t port,  IDataHandler* dataHandler):
+	    m_pDataHandler(dataHandler),
+	    m_uPort(port),
+	    m_strIp(ip),
+	    m_input(4*1024, 2*1024),
+	    m_output(4*1024, 2*1024),
+	    m_uLastWarn(0),
+	    m_uDropCount(0),
+	    m_bOverflow(false),
             fd(so),
             ev(new CEvSource(so, ep))
-			{
+	    {
                 ep->netEpollAdd(ev.get(), EPOLL_READABLE);
                 ev->setReadCallback(boost::bind(&CTcpConn::onRead, this));
                 ev->setWriteCallback(boost::bind(&CTcpConn::onWrite, this));
-			}
-			virtual ~CTcpConn();
+	    }
+	    virtual ~CTcpConn();
 
             void setConnStat(ConnectStat stat)
-		    {
-			    m_stat = stat;
-		    }
+	    {
+		m_stat = stat;
+            }
 
-		    ConnectStat getConnStat()
-		    {
-			    return m_stat;
-		    }
+	    ConnectStat getConnStat()
+	    {
+		return m_stat;
+	    }
 
             void setCloseCallback(const CloseCallback& cb)
             { 
                 closeCallback = cb; 
             }
 
-			uint32_t getPeerPort() 
-			{
-				return m_uPort;
-			}
+	    uint32_t getPeerPort() 
+	    {
+	        return m_uPort;
+	    }
 
-			string getPeerIp() 
-			{
-				return m_strIp;
-			}
+	    string getPeerIp() 
+	    {
+		return m_strIp;
+	    }
 
             int getFd()
             {
                 return fd;
             }
 
-			void filterRead(char* data, size_t size)
-			{
-			}
+	    void filterRead(char* data, size_t size)
+	    {
+	    }
 
-			void onRead();
-			void onWrite();
-			void onError(){}
+	    void onRead();
+	    void onWrite();
+	    void onError(){}
             
             void handleClose(TcpConnPtr conn);
 
             //此处可以解密数据， 如果是空的话就是传输明文， 可以重新解密规则，https？ 各位爷
             virtual void decryptData(){};
 
-			void send(const char* data, uint32_t len);
+	    void send(const char* data, uint32_t len);
 
-			void send(std::string& strMsg, uint32_t cmd, uint32_t requestId, uint64_t uid64);
-			/*客户端连接服务端回包接口*/
-			void sendResponse(google::protobuf::Message& msg, uint32_t cmd, uint32_t requestId, uint64_t uid64);
+	    void send(std::string& strMsg, uint32_t cmd, uint32_t requestId, uint64_t uid64);
+
+	    /*客户端连接服务端回包接口*/
+	    void sendResponse(google::protobuf::Message& msg, uint32_t cmd, uint32_t requestId, uint64_t uid64);
 			
-			void sendError(uint32_t cmd, uint32_t requestId, uint8_t error);	
+	    void sendError(uint32_t cmd, uint32_t requestId, uint8_t error);	
 
-		private:
+	private:
 				
-			int sendHelper(const char* data, size_t size);
+	    int sendHelper(const char* data, size_t size);
 
-			IDataHandler* m_pDataHandler;
+	    IDataHandler* m_pDataHandler;
 	
-			uint32_t m_uPort;
-			string   m_strIp;
+	    uint32_t m_uPort;
+	    string   m_strIp;
 				
-			SocketBuffer m_input;
-			SocketBuffer m_output;
+	    SocketBuffer m_input;
+	    SocketBuffer m_output;
 
-			uint32_t m_uLastWarn;
-			uint32_t m_uDropCount;
+	    uint32_t m_uLastWarn;
+	    uint32_t m_uDropCount;
 
-			bool m_bOverflow;
+	    bool m_bOverflow;
 
             ConnectStat m_stat;
 
